25bfdaf3b7a58c23fdc7c0d9221b555d5c64426e
Cosplay Diver <kaydeekhing@gmail.com>
2020-04-22 18:56:46 -0400

    schema defines PRIMARY KEY

--- db/schema.sql
+++ db/schema.sql
@@ -7,5 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR (100) NOT NULL,
-    devoured BOOLEAN
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
 ); 
